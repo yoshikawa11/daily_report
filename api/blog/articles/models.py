5a9945080e6bb85dab67f75266848e935dffa76a
yoshikawa masashi <gunterriote@yahoo.co.jp>
2021-01-23 19:14:06 +0900

    API作成

--- api/blog/articles/models.py
+++ api/blog/articles/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Todo(models.Model):
     uuid = models.UUIDField('uuid', primary_key=True, default=uuid.uuid4, editable=False)
-    content = models.CharField(verbose_name='todo', max_length=100)
-    priority = models.CharField(verbose_name='優先度', max_length=2)
+    content = models.CharField(verbose_name="todo", max_length=100)
+    priority = models.CharField(verbose_name="優先度", max_length=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
